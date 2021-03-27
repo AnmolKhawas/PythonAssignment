430ab65c91610b110324c5a9f2febaa95a593d29
AnmolKhawas <anmolkhawas1998@gmail.com>
2021-03-27 20:21:46 +0530

    commit

--- Assignment5/9.py
+++ Assignment5/9.py
@@ -13,4 +13,18 @@ for i in range(0,x):
     L2.append(ele)
 print("L2=",L2)
 
-L3
+L3=[]
+
+for i in L1:
+    for j in L2:
+        L3.append((i,j))
+print(L3)
+
+# or
+# l1=[1,2]
+# l2=[4,5]
+# l3=[]
+# for i in l1:
+#     for j in l2:
+#         l3.append((i,j))
+# print(l3)        
